48505e9f68df24c7438f8e98cb21e1b5c9928677
Ignacio Tolstoy <arkantos798@gmail.com>
2014-11-27 11:22:25 +0100

    First implementation of the search-track use case

---
+++ src/Naxhh/PlayCool/Application/UseCase/SearchTrackUseCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Naxhh\PlayCool\Application\UseCase;
+
+use Naxhh\PlayCool\Application\Contract\UseCase;
+use Naxhh\PlayCool\Application\Contract\Command;
+
+class SearchTrackUseCase implements UseCase
+{
+    private $track_repository;
+
+    public function __construct($track_repository) {
+        $this->track_repository = $track_repository;
+    }
+
+    public function handle(Command $command) {
+        $request = $command->getRequest();
+
+        $track_list = $this->track_repository->getListByName($request->get('search_term'));
+
+        return $track_list;
+    }
+}
---
+++ tests/Naxhh/Playcool/Application/UseCase/SearchTrackUseCaseTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Naxhh\Playcool\Application\UseCase;
+
+use Naxhh\Playcool\Application\Command\SearchTrackCommand;
+use Naxhh\Playcool\Domain\Entity\Track;
+use Naxhh\Playcool\Domain\Adapter\ArrayCollection;
+
+class SearchTrackUseCaseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSearchTrackReturnsAListOfTracks()
+    {
+        $track_list = new ArrayCollection(array(
+            Track::create('Track 1'),
+            Track::create('Amazing track')
+        ));
+
+        $track_repository = $this->getMock('TrackRepository', array('getListByName'));
+        $track_repository->expects($this->any())
+            ->method('getListByName')
+            ->will($this->returnValue($track_list));
+
+        $command  = new SearchTrackCommand('Amazing track');
+        $use_case = new SearchTrackUseCase($track_repository);
+
+        $track_list = $use_case->handle($command);
+
+        $this->assertCount(
+            2,
+            $track_list
+        );
+
+        $this->assertInstanceOf(
+            'Naxhh\Playcool\Domain\Entity\Track',
+            $track_list->get(1)
+        );
+    }
+}
