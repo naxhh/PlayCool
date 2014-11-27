ccdbfa557c6b10126ed1bc827bb12d9b3634192a
Ignacio Tolstoy <arkantos798@gmail.com>
2014-11-27 13:08:50 +0100

    Add the very first api method using silex

--- src/Naxhh/PlayCool/Presentation/Routes.php
+++ src/Naxhh/PlayCool/Presentation/Routes.php
@@ -1,7 +1,37 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-$app->get('/', function() use ($app) {
-    return new Response('Hi');
+/**
+ * Validates correct content-type header and converts JSON string to valid PHP array.
+ */
+$app->before(function(Request $request) {
+    if ($request->headers->get('Content-Type') !== 'application/json') {
+        return new JsonResponse(array('error' => 'Only application/json is accepted'), 415);
+    }
+
+    $json = json_decode($request->getContent(), true);
+    $request->request->replace(is_array($json) ? $json : array());
+
+});
+
+
+$app->post('/api/v1/playlists', function(Request $request) {
+    $name = $request->request->get('name');
+
+
+    $playlist_repository = new Naxhh\PlayCool\Infrastructure\Repository\Dummy\PlaylistRepository;
+    $command = new Naxhh\PlayCool\Application\Command\CreatePlaylistCommand($name);
+    $use_case = new Naxhh\PlayCool\Application\UseCase\CreatePlaylistUseCase($playlist_repository);
+
+    $playlist = $use_case->handle($command);
+
+    return new JsonResponse(
+        array(
+            'name' => $playlist->getName(),
+            'tracks' => $playlist->getTracks()
+        ),
+        201
+    );
 });
