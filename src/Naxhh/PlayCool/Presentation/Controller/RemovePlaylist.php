2bc3fc91b7b4586780d164eb2ad06a1204c9d2be
Ignacio Tolstoy <arkantos798@gmail.com>
2014-11-29 12:15:18 +0100

    Update name

--- src/Naxhh/PlayCool/Presentation/Controller/RemovePlaylist.php
+++ src/Naxhh/PlayCool/Presentation/Controller/RemovePlaylist.php
@@ -2,19 +2,16 @@
 
 namespace Naxhh\PlayCool\Presentation\Controller;
 
-use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Naxhh\PlayCool\Infrastructure\Repository\Dummy\PlaylistRepository;
 use Naxhh\PlayCool\Application\Command\RemovePlaylistCommand;
 use Naxhh\PlayCool\Application\UseCase\RemovePlaylistUseCase;
-use Naxhh\PlayCool\Presentation\Transformer\PlaylistTransformer;
-use League\Fractal;
 
 class RemovePlaylist
 {
-    public function execute(Request $request, Application $app, $id) {
+    public function execute(Request $request, $id) {
         $this->buildUseCase()->handle(new RemovePlaylistCommand($id));
 
         return new JsonResponse(array(), 204);
