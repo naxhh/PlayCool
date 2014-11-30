d30074bdba5be8c1d10647a08c3c4c8d4a3520d5
Ignacio Tolstoy <arkantos798@gmail.com>
2014-11-30 18:12:35 +0100

    Add redis

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@ $app = new Silex\Application();
 // Config.
 $app['debug'] = true;
 $app['files_path'] = __DIR__ . DS . '..' . DS . 'var' . DS;
+$app['redis.host'] = 'tcp://127.0.0.1:6379';
 
 // Services.
 require __DIR__ . DS . '..' . DS . 'src' . DS . 'Naxhh' . DS . 'PlayCool' . DS . 'Presentation' . DS . 'Services.php';
